bdf37dc08186a3da91eb5e94c570793d705b2543
Daniel Stangeland <47644255+D49ll@users.noreply.github.com>
2023-05-26 14:14:39 +0200

    Added contour version into same py file

--- detect_dices.py
+++ detect_dices.py
@@ -191,6 +191,65 @@ def overlay(frame, mask):
     image[mask==255] = (50,255,50)
     return image
 
+def getContours(frame, threshold_size=1500):
+
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_img = cv2.GaussianBlur(gray_img, (3,3), 0)
+    gray_img = cv2.GaussianBlur(gray_img, (3,3), 0)
+    gray_img = cv2.GaussianBlur(gray_img, (3,3), 0)
+
+    threshold_img = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 41, 9)
+
+    conts = cv2.erode(threshold_img, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5)), iterations=5)
+    imgContour = frame
+
+    contours, hierarchy = cv2.findContours(conts, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    areas = np.array([ cv2.contourArea(cnt) for cnt in contours ])
+    areas_over_threshold = np.where(threshold_size < areas)[0]
+    areas_under_thrshold = np.where(imgContour.shape[0]*imgContour.shape[1]*0.6 > areas)[0]
+    selected_areas = np.intersect1d(areas_over_threshold, areas_under_thrshold)
+    num_blobs = []
+    centers = []
+    dices = []
+
+    keypoints = get_blobs(frame)
+    
+    areas_over_threshold = selected_areas
+    for i in areas_over_threshold:
+        cnt = contours[i]
+        cv2.drawContours(imgContour, cnt, -1, (0,255,0),3)
+        
+        peri = cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+        x,y,w,h = cv2.boundingRect(approx)
+        center = (int(x + w/2), int(y + h/2))
+
+        #cv2.circle(imgContour, center, 15, (255, 0, 0), -1)
+        
+        
+        num_blob = 0
+        for keypoint in keypoints:
+            if(cv2.pointPolygonTest(cnt, (keypoint.pt), measureDist=False)>=0):
+                num_blob+=1
+        
+        #cv2.putText(imgContour, f"score {i} {num_blob}", center, cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,0,255), 2)
+        
+        num_blobs.append(num_blob)
+        centers.append(center)
+            
+            #Display dice values
+    for a_i, i in enumerate(areas_over_threshold):
+        s = True
+        for a_j, j in enumerate(areas_over_threshold):
+            if i != j and (cv2.pointPolygonTest(contours[i], (centers[a_j]), measureDist=False) >= 0) and num_blobs[a_j] > 0 and areas[i] > areas[j]:
+                s = False
+        
+        if s and num_blobs[a_i] > 0:
+            cv2.drawContours(imgContour, contours[i], -1, (0,255,255),6)
+            cv2.putText(imgContour, f"score {num_blobs[a_i]}", centers[a_i], cv2.FONT_HERSHEY_COMPLEX, 0.9, (0,0,255), 2)
+            dices.append(num_blobs)
+    return imgContour, dices
+
 def detect_dices_with_morph(cap, show_all=False, show_first_last=False):
     success, frame = cap.read()
     contour_image = frame.copy()
@@ -321,7 +380,6 @@ def yatzi():
                 out_frame, list_of_dices = blob(cap)
                 limit = 10
                 version_string = "Clustering"
-
             game_frame = Image.fromarray(np.zeros((720,400)))
             
             if len(list_of_dices)<5: 
